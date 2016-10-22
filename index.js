c7f2b8c4e4740030244fcc618574b2ed770919bf
Derick Quartey <niikwartey1@gmail.com>
2016-10-22 15:54:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+function Rectangle(sides, width, height) {
+  this.sides = sides;
+  this.width = width;
+  this.height = height;
+  
+  this.area = function() {
+    return this.width * this.height;
+  }
+  this.perimeter = function() {
+    return (this.width + this.height) * 2;
+  }
+}
+
+
